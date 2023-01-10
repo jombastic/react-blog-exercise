1f70930260de327d5bb47645f9f07378ea20690f
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-01-10 14:57:42 +0100

    add click events on buttons to log some strings and the event object

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    const handleClick = (event) => {
+        console.log('hello, ninjas', event);
+    } 
+
+    const handleClickAgain = (name, event) => {
+        console.log('hello, ' + name, event.target);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(event) => handleClickAgain('slavcho', event)}>Click me again</button>
         </div>
     );
 }
