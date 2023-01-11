8d284e0820607c57046658f467d121a415774495
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-01-11 12:04:24 +0100

    set a catch method to the fetch promise and an error state to display an error message

--- src/Home.js
+++ src/Home.js
@@ -4,16 +4,25 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
                 .then(response => {
+                    if (!response.ok)
+                        throw Error('Could not fetch the data for that resource');
+                        
                     // returns async data so we need another then below
                     return response.json()
                 })
                 .then(data => {
                     setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setError(err.message)
                     setIsPending(false);
                 })
         }, 1000);
@@ -21,6 +30,7 @@ const Home = () => {
 
     return (
         <div className="home">
+            { error && <div>{ error } </div>}
             {isPending && <div>Loading...</div>}
             {/* if blogs is true the html on the right side is evaluated and rendered */}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
