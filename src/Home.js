91f91195fe51faa0cdd48204d734a5db5a11754c
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-01-11 11:54:16 +0100

    set conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,25 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(response => {
-                // returns async data so we need another then below
-                return response.json()
-            })
-            .then(data => {
-                setBlogs(data);
-            })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(response => {
+                    // returns async data so we need another then below
+                    return response.json()
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false);
+                })
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {/* if blogs is true the html on the right side is evaluated and rendered */}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
